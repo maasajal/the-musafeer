52a4309b1def1a7590dadb4dfa4f0461b639a448
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2025-03-14 11:59:31 +0200

    Banner added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="font-[family-name:var(--font-geist-sans)]">
       <KaabaVideo />
-      {/* <Banner /> */}
+      <Banner />
     </main>
   );
 }
